021c86cc96592b6c3c5905fe6d3a606ea0729825
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-03 16:35:11 -0700

    Create Concatenation of Array.cpp

---
+++ Concatenation of Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+         int n=nums.size();
+     vector<int> ans(2*n);
+     for(int i=0 ; i<2*n;i++ ){
+  
+            if(i<n){
+               ans[i]=nums[i];
+            }
+            else{
+                 ans[i]=nums[i-n] ;
+            }
+        
+     } 
+      return ans;
+    }
+};
