d8f3fe91a3060b503465fc7bc7aeb5904d7f506e
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-04 07:48:25 -0700

    Update Final Value of Variable After Performing Operations.cpp

--- Final Value of Variable After Performing Operations.cpp
+++ Final Value of Variable After Performing Operations.cpp
@@ -1,1 +1,20 @@
-Final Value of Variable After Performing Operations
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+         int n= operations.size();
+        int value=0;
+   
+        for(int i=0; i<n ;i++){
+        string temp=operations[i];
+         if( temp=="++X" || temp=="X++" ){
+            value++;
+         }
+
+         else if(temp=="--X" || temp=="X--"){
+            value--;
+         }
+        }
+
+        return value;
+    }
+};
