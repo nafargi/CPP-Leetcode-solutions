cbb7fbb16b9fb2c66498f52d521397515dc97be6
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-06 13:03:16 -0700

    Create Find Words Containing Character.cpp

---
+++ Find Words Containing Character.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    vector<int> findWordsContaining(vector<string>& words, char x) {
+        vector<int> numList ;
+
+        for(int i=0; i<words.size(); i++){
+          string  charArray= words[i];
+            for(int j=0 ; j<charArray.length() ; j++){
+                
+                 if(charArray[j]==x){
+                  numList.push_back(i);
+                    break;
+                 }
+            }
+        }
+        return numList;
+    }
+};
