ccbaa810663e55a2da312058d551c9bde0ceaef1
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-09 12:49:57 -0700

    Update Update Maximum Number of Words Found in Sentences.cpp

--- Update Maximum Number of Words Found in Sentences.cpp
+++ Update Maximum Number of Words Found in Sentences.cpp
@@ -1,1 +1,11 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+    int maxWord=0;
+    for (size_t i=0; i< sentences.size();i++){
+        int counter=count(sentences[i].begin(),sentences[i].end(),' ')+1;
         maxWord=max(maxWord,counter);
+    }  
+     return maxWord;
+    }
+};
