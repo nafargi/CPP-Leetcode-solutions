92a805e4cfaec4b91de218cb78ae1b8b285dccf5
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-10 17:15:48 -0700

    Update Decompress Run-Length Encoded List.cpp

--- Decompress Run-Length Encoded List.cpp
+++ Decompress Run-Length Encoded List.cpp
@@ -1,18 +1,13 @@
 // this code Beats 100.00% of users with C++
 class Solution {
-    public int[] decompressRLElist(int[] nums) {
-        int sizeOfArray=0;
-        for(int i=0; i<nums.length; i+=2){
-           sizeOfArray+=nums[i];
-        }
-        int[] concatenatedArray = new int[sizeOfArray];
-        
-        int index=0;
-        for(int i=0; i<nums.length; i+=2){
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> concatenatedArray;
+        for(int i=0; i<nums.size(); i+=2){
              for(int j=0; j<nums[i]; j++){
-                concatenatedArray[index++] = nums[i+1];
+               concatenatedArray.emplace_back(nums[i+1]);
              }
         }
         return concatenatedArray;
     }
-}
+};
