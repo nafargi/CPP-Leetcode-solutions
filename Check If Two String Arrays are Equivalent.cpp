d38434aaef2784e7601ca61e9f9020651ec036ad
Nafargi Damena Geda <143782082+nafargi@users.noreply.github.com>
2024-05-20 00:14:25 -0700

    Update Check If Two String Arrays are Equivalent.cpp

--- Check If Two String Arrays are Equivalent.cpp
+++ Check If Two String Arrays are Equivalent.cpp
@@ -4,6 +4,7 @@ public:
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
        string stringWord1;
        string stringWord2;
+        
        for(int i=0; i<word1.size(); i++){
             for(int j=0; j<word1[i].size(); j++){
                stringWord1.push_back(word1[i][j]);
