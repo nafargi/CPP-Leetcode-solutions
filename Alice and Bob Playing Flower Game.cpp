247a2a7ca0c6c4fa09c418f8b4906f1269212b22
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-05-22 10:11:10 +0300

    Create Alice and Bob Playing Flower Game.cpp

---
+++ Alice and Bob Playing Flower Game.cpp
@@ -0,0 +1,10 @@
+class Solution {
+ public:
+  long long flowerGame(int n, int m) {
+    const int xEven = n / 2;
+    const int yEven = m / 2;
+    const int xOdd = (n + 1) / 2;
+    const int yOdd = (m + 1) / 2;
+    return 1L * xEven * yOdd + 1L * yEven * xOdd;
+  }
+};
