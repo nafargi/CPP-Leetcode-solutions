de4ac8b5876efd011f97be03ecb38f73dd52ec53
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-05-23 16:16:45 +0300

    Create Count Negative Numbers in a Sorted Matrix.cpp

---
+++ Count Negative Numbers in a Sorted Matrix.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int counter = 0;
+        int rowNum = grid.size();
+        int colNum = grid[0].size();
+
+        for(const auto& row : grid){
+            int left = 0; int right = colNum-1;
+
+            while( left <= right ){
+                 int midNum = left + (right - left)/2;
+                 if(row[midNum] < 0){
+                    right = midNum - 1;
+                 }else{
+                    left = midNum + 1;
+                 }
+            }
+            counter += colNum - left;
+        }
+        return counter;
+    }
+};
