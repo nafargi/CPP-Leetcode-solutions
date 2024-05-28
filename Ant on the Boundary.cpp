28264decafd31e577881ba1b55841c0f392e9ebf
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-05-29 01:04:10 +0300

    Update Ant on the Boundary.cpp

--- Ant on the Boundary.cpp
+++ Ant on the Boundary.cpp
@@ -1,5 +1,5 @@
 class Solution {
- public:111
+ public:11
   int returnToBoundaryCount(vector<int>& nums) {
     partial_sum(nums.begin(), nums.end(), nums.begin());
     return ranges::count(nums, 0);
