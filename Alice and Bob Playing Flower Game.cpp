71e55c094b207f0c521e0dae5d06355b5e66ab3c
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-09-04 01:02:09 +0300

    Update Alice and Bob Playing Flower Game.cpp

--- Alice and Bob Playing Flower Game.cpp
+++ Alice and Bob Playing Flower Game.cpp
@@ -4,8 +4,7 @@ class Solution {
     const int xEven = n / 2;
     const int yEven = m / 2;
     const int xOdd = (n + 1) / 2;
-    const int yOdd = (m + 1) / 2;
-   
+    const int yOdd = (m + 1) / 2; 
     return 1L * xEven * yOdd + 1L * yEven * xOdd;
   }
 };
