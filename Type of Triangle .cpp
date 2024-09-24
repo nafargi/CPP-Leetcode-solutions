2c833e0a6bbfa76dd08ac6a1f51797a8b7728775
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-09-24 17:01:59 +0300

    Update Type of Triangle .cpp

--- Type of Triangle .cpp
+++ Type of Triangle .cpp
@@ -2,7 +2,6 @@ class Solution {
  public:
   string triangleType(vector<int>& nums) {
     ranges::sort(nums);
-   
     if (nums[0] + nums[1] <= nums[2])
       return "none";
     if (nums[0] == nums[1] && nums[1] == nums[2])
