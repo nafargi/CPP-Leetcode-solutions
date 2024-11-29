157c37200fd3e9954725b88f22813d29d4d8757b
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-11-29 22:51:11 +0300

    Update How Many Numbers Are Smaller Than the Current Number.cpp

--- How Many Numbers Are Smaller Than the Current Number.cpp
+++ How Many Numbers Are Smaller Than the Current Number.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
            int n= nums.size();
-        
       vector<int>  ans (n);
         for(int i=0;i<n; i++){
             int counter=0;
