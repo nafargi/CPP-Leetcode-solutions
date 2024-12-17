17dc8222970421905f1750c6e294a1ba4b9954c0
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2024-12-17 23:39:19 +0300

    Create  Maximum Average Subarray I.cpp

---
+++ Maximum Average Subarray I.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double maxNum = -DBL_MAX;
+        int sum =0;
+        for(int i=0; i<k; i++){
+             sum += nums[i];
+        }
+         
+      maxNum = sum / (double)k;
+      int startIndex=0;
+      int endIndex = k;
+
+      while(endIndex < nums.size()){
+         sum -=nums[startIndex];
+         sum += nums[endIndex];
+        
+         startIndex++;
+        endIndex++;
+        
+         maxNum = max(maxNum,sum / (double)k);
+      }
+      return maxNum;
+    }
+};
