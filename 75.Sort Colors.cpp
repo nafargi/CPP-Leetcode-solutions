e8d9a4968bde798d77e9678092a9bc340222fa48
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-05-17 21:54:27 +0300

    Create 75.Sort Colors.cpp

---
+++ 75.Sort Colors.cpp
@@ -0,0 +1,7 @@
+    void sortColors(vector<int>& nums) {
+    int low{}, mid{}, high{nums.size() - 1};
+    while (mid <= high)
+        if (nums[mid] == 0) swap(nums[mid++], nums[low++]);
+        else if (nums[mid] == 1) mid++;
+        else swap(nums[mid], nums[high--]);
+}
