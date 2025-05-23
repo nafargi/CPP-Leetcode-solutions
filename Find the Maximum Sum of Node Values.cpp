04d2c5bbb11cab203d8aafe751d3f491b7ce1816
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-05-23 21:10:16 +0300

    this is an amazing achievement 265 strike question

---
+++ Find the Maximum Sum of Node Values.cpp
@@ -0,0 +1,22 @@
+// this code has got 100% acceptance on leetcode
+
+class Solution {
+public:
+    long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
+        long maxSum = 0;
+        int changedCount = 0;
+        int minDiff = INT_MAX;
+
+    for (const int num : nums){
+
+        maxSum += max(num,num^k);
+        changedCount += ((num^k) > num) ? 1 : 0;
+        minDiff = min(minDiff, abs(num - (num^k)));
+    }
+
+    if (changedCount % 2 == 0)
+      return maxSum;
+    return maxSum - minDiff;
+
+    }
+};
