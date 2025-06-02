d49cc093031451a317e311380273aba70610d82f
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-06-02 21:33:51 +0300

    Create Candy.cpp

---
+++ Candy.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size(); 
+
+        vector<int> cand(n, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                cand[i] = cand[i - 1] + 1;
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                if (cand[i] <= cand[i + 1]) {
+                    cand[i] = cand[i + 1] + 1;
+                }
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += cand[i];
+        }
+
+        return ans;
+    }
+};
