dfcc040b9c5eb675507e144c4a70f8bc2de533d9
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-06-03 22:51:37 +0300

    Maximum Candies You Can Get from Boxes  276 strike days on leetcode.cpp
    consistency matters

---
+++ . Maximum Candies You Can Get from Boxes  276 strike days on leetcode.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int maxCandies(vector<int>& status, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        int ans = 0;
+        queue <int> q;
+        vector <bool> reachedClosedBoxes(status.size());
+
+        auto pushBoxesIfPossible = [&status,&q , &reachedClosedBoxes](const vector<int> &boxes){
+            for (const int box: boxes)
+              if(status[box])
+               q.push(box);
+              else
+                reachedClosedBoxes[box] = true;
+        };
+
+      pushBoxesIfPossible(initialBoxes);
+
+      while(!q.empty()){
+        const int currBox = q.front();
+        q.pop();
+
+        ans += candies[currBox];
+
+        for (const int key :keys[currBox]){
+            if (!status[key] && reachedClosedBoxes[key])
+                q.push(key);
+            status[key] = 1;
+
+        }
+        pushBoxesIfPossible(containedBoxes[currBox]);
+
+      }
+
+      return ans;
+    }
+};
