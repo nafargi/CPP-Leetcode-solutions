f1847dc9402870c985d754834e8032b0b6a7284e
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-06-18 22:25:35 +0300

    Create  Divide Array Into Arrays With Max Difference.cpp

---
+++ Divide Array Into Arrays With Max Difference.cpp
@@ -0,0 +1,36 @@
+// version for not using push_back
+class Solution {
+public:
+    static vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        int n=nums.size();
+        int freq[100001]={0};
+        int xMax=0;
+        for(int x: nums){
+            freq[x]++;
+            xMax=max(x, xMax);
+        }
+        vector<vector<int>> ans(n/3);
+        vector<int> vec3(3);
+        int count=0, vec_m, i=0;
+        for(int x=1; x<=xMax; x++){
+            for( ; freq[x]>0; freq[x]--, count++){
+                if (count%3==0)
+                    vec_m=x;
+                if (x>vec_m+k ) return {};
+                vec3[count]=x;
+                if (count%3==2){
+                    ans[i++]=vec3;
+                    count=-1;
+                }
+            }
+        }
+        return ans;
+    }
+};
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
