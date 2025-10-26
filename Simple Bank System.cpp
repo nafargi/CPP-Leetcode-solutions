095ca4825376b800deba8f54ae051a4d55117c43
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2025-10-26 20:46:08 +0300

    Add Simple Bank System class with basic operations
    Implement a simple bank system with deposit, withdraw, and transfer methods.

---
+++ Simple Bank System.cpp
@@ -0,0 +1,45 @@
+class Bank {
+public:
+    Bank(vector<long long>& balance) : balance(std::move(balance)) {}
+    
+    bool transfer(int account1, int account2, long long money) {
+        if(!isValid(account2))
+          return false;
+        return  withdraw(account1, money) && deposit(account2, money);
+
+    }
+    
+    bool deposit(int account, long long money) {
+        if(!isValid(account))
+          return false;
+        balance[account - 1] += money;
+        return true;
+    }
+    
+    bool withdraw(int account, long long money) {
+        if(!isValid(account))
+          return false;
+        if(balance[account - 1] < money)
+           return false;
+        balance[account - 1] -= money;
+        return true;
+    }
+
+private:
+    vector<long long> balance;
+
+    bool isValid(int account){
+        return 1 <= account && account <= balance.size();
+    }
+
+};
+
+
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */
