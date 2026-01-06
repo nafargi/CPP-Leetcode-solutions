5dd74dcebafbd4e231eb2dcc35aa9202070be8bd
Nafargi Damena <143782082+nafargi@users.noreply.github.com>
2026-01-06 23:18:49 +0300

    Create 1161. Maximum Level Sum of a Binary Tree.cpp

---
+++ 1161. Maximum Level Sum of a Binary Tree.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxLevelSum(TreeNode* root) {
+        if (!root) return 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        int level = 1;
+        int answerLevel = 1;
+        long long maxSum = LLONG_MIN;
+
+        while (!q.empty()) {
+            int size = q.size();
+            long long levelSum = 0;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                levelSum += node->val;
+
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+
+            if (levelSum > maxSum) {
+                maxSum = levelSum;
+                answerLevel = level;
+            }
+
+            level++;
+        }
+
+        return answerLevel;
+
+    }
+};
